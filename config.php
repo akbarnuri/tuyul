8c670895d8861eaf36784857dca31f332389cde1
akbarnuri <makerctvvip@gmail.com>
2018-12-20 01:34:13 +0700

    project lama

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh5rQu7qSo7_Lfpayiphof3au3IK5gWCzm4mpkX3Ay4qKfMywupuvtbWY2a2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "fe71cc69ad3df502";
+$os="android";
+$sign="06c4cf185df8d7878185770e30a3b5c7fe78c5ea62c4d5288a78bb7fcb3ae3dd";
